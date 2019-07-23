36450304334c0950e655a69de54fb874cc81ff10
Javier Evans <jevans@pagerduty.com>
2019-07-22 21:33:51 -0700

    03-start-controller

--- lib/get_togather/application.ex
+++ lib/get_togather/application.ex
@@ -5,11 +5,53 @@ defmodule GetTogather.Application do
 
   use Application
 
+  alias GetTogather.Meetups.Group
+
+  @initial_state [
+    %Group{
+      name: "Seattle Elixir Meetup",
+      id: 1,
+      inserted_at: ~N[2018-01-06 23:00:04],
+      updated_at: ~N[2018-01-06 23:00:04]
+    },
+    %Group{
+      name: "Portland Elixir Meetup",
+      id: 2,
+      inserted_at: ~N[2018-02-06 23:00:04],
+      updated_at: ~N[2018-02-06 23:00:04]
+    },
+    %Group{
+      name: "East Olympia Snake Painters",
+      id: 3,
+      inserted_at: ~N[2018-04-06 23:00:04],
+      updated_at: ~N[2018-04-06 23:00:04]
+    },
+    %Group{
+      name: "Tacoma Gut Bucket Enthusiasts United",
+      id: 4,
+      inserted_at: ~N[2018-06-06 23:00:04],
+      updated_at: ~N[2018-06-06 23:00:04]
+    },
+    %Group{
+      name: "Tumwater Turtle Identification Collective",
+      id: 5,
+      inserted_at: ~N[2018-08-06 23:00:04],
+      updated_at: ~N[2018-08-06 23:00:04]
+    },
+    %Group{
+      name: "Seattle Bowler Hat Aficionados",
+      id: 6,
+      inserted_at: ~N[2018-10-06 23:00:04],
+      updated_at: ~N[2018-10-06 23:00:04]
+    }
+  ]
+
   def start(_type, _args) do
     # List all child processes to be supervised
     children = [
       # Start the endpoint when the application starts
-      GetTogatherWeb.Endpoint
+      GetTogatherWeb.Endpoint,
+      {GetTogather.Storage, %{groups: @initial_state}}
       # Starts a worker by calling: GetTogather.Worker.start_link(arg)
       # {GetTogather.Worker, arg},
     ]
--- test/get_togather/meetups_test.exs
+++ test/get_togather/meetups_test.exs
@@ -5,5 +5,22 @@ defmodule GetTogather.MeetupsTest do
 
   describe "groups" do
     alias GetTogather.Meetups.Group
+
+    test "list_groups/0 returns the most recently updated three groups by default" do
+      expected = ["Seattle Elixir Meetup", "Portland Elixir Meetup", "East Olympia Snake Painters"]
+
+      assert expected = Meetups.list_groups() |> Enum.map(&Map.get(&1, :name))
+    end
+
+    test "get_group/1 returns the group with the correct_id" do
+      expected = %Group{
+        name: "Seattle Elixir Meetup",
+        id: 1,
+        inserted_at: ~N[2018-01-06 23:00:04],
+        updated_at: ~N[2018-01-06 23:00:04]
+      }
+
+      assert expected == Meetups.get_group(1)
+    end
   end
 end
