beb0053dae85fcb581ffd3d7f7763d40fa0c9324
Javier Evans <jevans@pagerduty.com>
2019-07-22 21:33:51 -0700

    03-start-controller

--- lib/get_togather/meetups/meetups.ex
+++ lib/get_togather/meetups/meetups.ex
@@ -2,4 +2,16 @@ defmodule GetTogather.Meetups do
   @moduledoc """
   The Meetups context.
   """
+  alias GetTogather.Storage
+
+  def list_groups() do
+    Storage.get_resource(:groups)
+    |> Enum.sort(fn g1, g2 -> g1.updated_at <= g2.updated_at end)
+    |> Enum.take(3)
+  end
+
+  def get_group(id) do
+    Storage.get_resource(:groups)
+    |> Enum.find(fn group -> group.id == id end)
+  end
 end
